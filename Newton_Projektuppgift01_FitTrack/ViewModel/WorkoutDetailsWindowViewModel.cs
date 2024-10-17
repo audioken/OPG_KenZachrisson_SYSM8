9548528b56faaf8fe642cc7b0474e483241e046d
audioken <kenzachrisson@gmail.com>
2024-10-17 10:44:49 +0200

    Skapat alla mappar och filer. Även lagt till logiken för RelayCommand och ViewModelBase.

---
+++ Newton_Projektuppgift01_FitTrack/ViewModel/WorkoutDetailsWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton_Projektuppgift01_FitTrack.ViewModel
+{
+    class WorkoutDetailsWindowViewModel
+    {
+    }
+}
