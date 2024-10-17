08d5cbe2691fecd1b15cc632d92f579d147ea996
audioken <kenzachrisson@gmail.com>
2024-10-17 22:30:04 +0200

    MainWindow: * Skapade kontroll vid inloggning. Testade skapa konto att logga in med. Funkade bra.

--- Newton_Projektuppgift01_FitTrack/Model/User.cs
+++ Newton_Projektuppgift01_FitTrack/Model/User.cs
@@ -6,9 +6,18 @@
         public string SecurityQuestion { get; set; }
         private string SecurityAnswer { get; set; }
 
+        // Anropas vid registrering av ny användare
         public User(string UserName, string Password, string Country) : base(UserName, Password)
         {
             this.Country = Country;
+        }
+
+        // Anropas när all information om användaren ska med
+        public User(string UserName, string Password, string Country, string SecurityQuestion, string SecurityAnswer) : base(UserName, Password)
+        {
+            this.Country = Country;
+            this.SecurityQuestion = SecurityQuestion;
+            this.SecurityAnswer = SecurityAnswer;
         }
 
         public override void SignIn()
--- Newton_Projektuppgift01_FitTrack/ViewModel/MainWindowViewModel.cs
+++ Newton_Projektuppgift01_FitTrack/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Newton_Projektuppgift01_FitTrack.MVVM;
+﻿using Newton_Projektuppgift01_FitTrack.Model;
+using Newton_Projektuppgift01_FitTrack.MVVM;
 using Newton_Projektuppgift01_FitTrack.View;
 using System.Windows;
 
@@ -19,14 +20,46 @@ namespace Newton_Projektuppgift01_FitTrack.ViewModel
         public RelayCommand RegisterCommand => new RelayCommand(execute => Register());
         public RelayCommand ForgottPasswordCommand => new RelayCommand(execute => ForgotPassword()); // För VG
 
+        // KONSTRUKTOR ↓
+        public MainWindowViewModel()
+        {
+            // Skapar en tillfällig User för testning
+            Manager.Instance.AllUsers.Add(new User("user", "password", "nocountry"));
+        }
 
         // METODER ↓
         // Kontroll för inloggning
         public void SignIn()
         {
-            if (UsernameInput != null && PasswordInput != null)
-            {
+            // Kontroll om kontot finns
+            bool accountFound = false;
 
+            // Kolla så text är inmatad
+            if (!string.IsNullOrEmpty(UsernameInput) && !string.IsNullOrEmpty(PasswordInput))
+            {
+                // Kollar igenom användarlistan i Managerklassen
+                foreach (User user in Manager.Instance.AllUsers)
+                {
+                    // Kontrollerar så det matchar en användarprofil
+                    if (UsernameInput == user.UserName && PasswordInput == user.Password)
+                    {
+                        // Testutskrift
+                        MessageBox.Show("Jodå vi hittade dig!");
+                        accountFound = true; // Undviker att skriva ut felmeddelandet efter foreach-loopen
+                        break;
+                    }
+                }
+
+                // Felmeddelande om inget konto matchar
+                if (accountFound == false)
+                {
+                    MessageBox.Show("Användarnamn och/eller lösenord är fel..");
+                }
+            }
+            // Felmeddelande om textboxarna är tomma
+            else
+            {
+                MessageBox.Show("Inget har skrivits in!");
             }
         }
 
