c5c08d7dc463fd0be940ec053d24bb7cc7ead69f
audioken <kenzachrisson@gmail.com>
2024-10-17 09:53:34 +0200

    Add project files.

---
+++ Newton_Projektuppgift01_FitTrack/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Newton_Projektuppgift01_FitTrack
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
