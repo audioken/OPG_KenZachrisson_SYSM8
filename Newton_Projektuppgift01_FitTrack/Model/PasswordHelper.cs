d349251b6b62ba008b1bd3ef270336ffcdae4872
audioken <kenzachrisson@gmail.com>
2024-10-31 00:46:24 +0100

    MainWindow, RegisterWindow, UserDetailsWindow: * Lyckades lösa Passwordbox, men med MVVM. Hittade en populär lösning där man tar hjälp av en annan klass och sen binder i XAML. Funkar fint

---
+++ Newton_Projektuppgift01_FitTrack/Model/PasswordHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Newton_Projektuppgift01_FitTrack.Model
+{
+
+    public static class PasswordHelper
+    {
+        public static readonly DependencyProperty BoundPasswordProperty =
+            DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordHelper), new FrameworkPropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+        public static string GetBoundPassword(DependencyObject d)
+        {
+            return (string)d.GetValue(BoundPasswordProperty);
+        }
+
+        public static void SetBoundPassword(DependencyObject d, string value)
+        {
+            d.SetValue(BoundPasswordProperty, value);
+        }
+
+        private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is PasswordBox passwordBox)
+            {
+                passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+
+                if ((string)e.NewValue != passwordBox.Password)
+                {
+                    passwordBox.Password = (string)e.NewValue;
+                }
+
+                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+            }
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox passwordBox)
+            {
+                SetBoundPassword(passwordBox, passwordBox.Password);
+            }
+        }
+    }
+
+}
